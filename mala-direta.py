094b777ec18277a4e53481d816b9176c411b5ba8
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:54:02 -0300

    Create mala-direta.py

---
+++ mala-direta.py
@@ -0,0 +1,9 @@
+# Faça um programa em Python que receba o nome do cliente, o dia de vencimento, o mês de vencimento e o valor da fatura  e imprima a mensagem com os dados recebidos.
+cliente=input("Digite o nome do cliente: ")
+diavencimento=input("Digite o dia de vencimento: ")
+mesvencimento=input("Digite o mês de vencimento: ")
+fatura=input("Digite o valor da fatura: ")
+print("Olá,",cliente,".")
+print("A sua fatura com vencimento em",diavencimento,"de",mesvencimento,"no valor de R$",fatura,"está fechada.")
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
